8adb66886e2ad2385d0b6dc8542cffcea165c150
widyaherlina9 <widyaerlina9@gmail.com>
2024-10-16 18:42:52 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Widya Herlina Br.Tambunan'; // TODO: replace this
+
+const studentId = 'FS10714451'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
